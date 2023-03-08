5cb5993c064c5c0c2e7fb463bf55aaab59a3bc48
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-03-08 14:05:23 +0200

    defined 3 functions

--- main.py
+++ main.py
@@ -11,8 +11,27 @@ def filter_CSV(filter_field, value):
     return list(filtered)
 
 
+def count_articles(key, value):
+    return len(filter_CSV(key, value))
+
+def is_article(key, value):
+    return len(filter_CSV(key, value)) != 0
+
+def longest_article(key, value):
+    filtered = filter_CSV(key, value)
+    maxObj = filtered[0]
+    maxPages = int(filtered[0]['pages'])
+    for obj in filtered:
+        if (int(obj['pages']) > maxPages):
+            maxPages = int(obj['pages'])
+            maxObj = obj
+    return maxObj
+
 print("Articles with a title of t4:")
 print(filter_CSV("title", "t4"))
 print('')
 print("Articles of a1 author:")
 print(filter_CSV("author", "a1"))
+print(count_articles('author', 'a1'))
+print(is_article('title', 't1'))
+print(longest_article('author', 'a1'))
